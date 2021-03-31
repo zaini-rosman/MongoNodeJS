05d5a0a26ebd9a9ddc1d4c3e85b345beaf77aea9
zaini <zainirsmn@gmail.com>
2021-03-31 17:44:37 +0800

    connect to mongo

---
+++ main.js
@@ -0,0 +1,55 @@
+const {MongoClient} = require('mongodb');
+async function main() {
+	// we'll add code here soon
+}
+/**
+ * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+ * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ */
+ const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+ const client = new MongoClient(uri);
+ await client.connect();
+ await listDatabases(client);
+ try {
+    await client.connect();
+
+    await listDatabases(client);
+ 
+} catch (e) {
+    console.error(e);
+}
+finally {
+    await client.close();
+}
+main().catch(console.error);
+async function main(){
+    /**
+     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+     */
+    const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+ 
+
+    const client = new MongoClient(uri);
+ 
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+ 
+        // Make the appropriate DB calls
+        await  listDatabases(client);
+ 
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
